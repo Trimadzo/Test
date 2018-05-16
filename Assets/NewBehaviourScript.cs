d769f43fedf8f7454b5a5c0a5515e174a1fa853f
Trimadzo <trimadzo@gmail.com>
2018-05-16 11:42:59 +0200

    Prova 2

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        print("Prova");
+        print("PRova 2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
